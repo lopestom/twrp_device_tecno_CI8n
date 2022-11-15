0b9567a45392f927e5056a8d9270d5b4c3eb1294
lopestorm <guillopes2002@gmail.com>
2022-11-15 20:17:21 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := device/tecno/TECNO-CI8n
+LOCAL_PATH := device/tecno/CI8n
 
 # Inherit from those products. Most specific first.
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
