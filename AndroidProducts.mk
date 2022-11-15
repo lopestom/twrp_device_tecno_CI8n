818bc9d3b7c3334fda096dbabb2ee5633c6bcedc
lopestorm <guillopes2002@gmail.com>
2022-11-15 15:47:33 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -18,7 +18,7 @@
 PRODUCT_MAKEFILES := \
      $(LOCAL_DIR)/twrp_TECNO-CI8n.mk
 
-COMMON_LUNCH_CHOICES := \
-    twrp_TECNO-CI8n-userdebug \
-    twrp_TECNO-CI8n-user \
-    twrp_TECNO-CI8n-eng
+COMMON_LUNCH_CHOICES := twrp_TECNO-CI8n-eng
+#    twrp_TECNO-CI8n-eng \
+#    twrp_TECNO-CI8n-userdebug \
+#    twrp_TECNO-CI8n-user
