7c1b2f2d697327ccfe206777ec0353b5a7df31ef
lopestorm <guillopes2002@gmail.com>
2023-07-07 17:25:05 -0300

    Update apply-patches.sh

--- patches /apply-patches.sh
+++ patches /apply-patches.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ./bootable/recovery
-patch -p1 < ../../twrp/device/tecno/CI8n/patches/0001-revert-save-settings-file-in-persist.patch
+patch -p1 < ../../device/tecno/CI8n/patches/0001-revert-save-settings-file-in-persist.patch
 cd ../..
