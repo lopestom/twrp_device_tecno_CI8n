8403031fc41ee144613081aafb3c098097836111
lopestorm <guillopes2002@gmail.com>
2023-07-08 00:09:14 -0300

    Update apply-patches.sh

--- patches /apply-patches.sh
+++ patches /apply-patches.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd ./bootable/recovery
+cd bootable/recovery
 patch -p1 < /home/runner/work/twrp_device_tecno_CI8n/twrp_device_tecno_CI8n/workspace/device/tecno/CI8n/patches/0001-revert-save-settings-file-in-persist.patch
 cd ../..
